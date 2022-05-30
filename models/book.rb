ba2518519a3504b6774d9dbf4963276310321a64
JuanLP <juan.luis.palacios.p@gmail.com>
2022-05-30 17:52:47 -0500

    Add Book class properties

--- models/book.rb
+++ models/book.rb
@@ -1,3 +1,8 @@
 require './item'
 class Book < Item
+  def initialize(date, archived, publisher, cover_state, id: nil)
+    super(date, archived, id: id)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
 end
