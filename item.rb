72eb380ee49952134831a867f2dd170897a5ae56
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-05-30 15:19:45 -0500

    Add method setters to Item class

--- item.rb
+++ item.rb
@@ -1,11 +1,27 @@
+# frozen_string_literal: true
+
 class Item
-  def initialize(genre, author, source, label, date, archived, id: nil)
+  attr_accessor :author, :genre, :source, :label
+
+  def initialize(date, archived, id: nil)
     @id = id.nil? ? Time.now.to_i : id
-    @genre = genre
-    @author = author
-    @source = source
-    @label = label
     @publish_date = date
     @archived = archived
+  end
+
+  def add_author(author)
+    @author = author
+  end
+
+  def add_genre(genre)
+    @genre = genre
+  end
+
+  def add_source(source)
+    @source = source
+  end
+
+  def add_label(label)
+    @label = label
   end
 end
