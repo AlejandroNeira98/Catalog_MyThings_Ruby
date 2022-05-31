949b0eeccb48600a4e5342bf6fd78c34cadc65aa
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-05-31 10:04:21 -0500

    Update Movie class

--- movie.rb
+++ movie.rb
@@ -1,9 +1,9 @@
 require_relative './item'
 
 class Movie < Item
-  def initialize(silet)
-    super
-    @silet = silet
+  def initialize(date, archived, silent, id: nil)
+    super(date, archived, id)
+    @silent = silent
   end
 
   def can_be_archived?
