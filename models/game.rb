1db1921c441be05dc0f96ae2c2b207ab3c944b14
CRClothier <c.r.clothier@gmail.com>
2022-05-31 11:27:58 -0500

    Fix linters

--- models/game.rb
+++ models/game.rb
@@ -2,7 +2,7 @@ require ',/item'
 
 class Game < item
   attr_accessor :multiplayer, :last_played_at
-  
+
   def initialize(date, archived, multiplayer, last_played_at, id: nil)
     super(date, archived, id: id)
     @multiplayer = multiplayer
