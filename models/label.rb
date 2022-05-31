be2a493e85617e468e474ff1c92cce6719447a81
JuanLP <juan.luis.palacios.p@gmail.com>
2022-05-31 15:53:01 -0500

    Add label class properties

--- models/label.rb
+++ models/label.rb
@@ -1,5 +1,11 @@
 class Label
-  def initialize()
-    
+  attr_accessor :title, :color
+  attr_reader :id, :items
+
+  def initialize(title, color, id: nil)
+    @id = id.nil? ? Time.now.to_i : id
+    @title = title
+    @color = color
+    @items = []
   end
 end
