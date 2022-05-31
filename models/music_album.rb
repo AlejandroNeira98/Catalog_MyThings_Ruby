290801576f2794b2d910bd2c3f344962ff325ab6
Saadat Ali <saadatlucky313@gmail.com>
2022-05-31 05:02:46 +0500

    Initialize Constructor

--- models/music_album.rb
+++ models/music_album.rb
@@ -1,5 +1,10 @@
-require './item'
+require_relative '../item'
 
 class MusicAlbum < Item
-  
+  attr_accessor :on_spotify
+
+  def initialize(date, archived, on_spotify, id: nil)
+    super(date, archived, id)
+    @on_spotify = on_spotify
+  end
 end
