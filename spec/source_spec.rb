3d8835feebdf05a81511a711d9bd7de0015fd3ec
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-05-31 16:15:17 -0500

    Change scturcture of tests

--- spec/source_spec.rb
+++ spec/source_spec.rb
@@ -2,13 +2,12 @@ require_relative '../source'
 require_relative '../item'
 
 describe Source do
-  before :all do
-    @source = Source.new('Some name')
-    @item = Item.new('date', true)
-    @source.add_item(@item)
-  end
-
   context 'Testing Source class add_source method' do
+    before :all do
+      @source = Source.new('Some name')
+      @item = Item.new('date', true)
+      @source.add_item(@item)
+    end
     it 'should add the item to items property in source' do
       expect(@source.items).to include(@item)
     end
