e148eeb5290f99a8afd5acb37d5179a1b27180e8
Saadat Ali <saadatlucky313@gmail.com>
2022-05-31 05:28:52 +0500

    Define Genre Class Properties

--- models/genre.rb
+++ models/genre.rb
@@ -1,5 +1,11 @@
 require_relative '../item'
 
-class Genre < Item
-  
+class Genre
+  attr_writer :items
+
+  def initialize(name, id: nil)
+    @id = id.nil? ? Time.now.to_i : id
+    @name = name
+    @items = []
+  end
 end
