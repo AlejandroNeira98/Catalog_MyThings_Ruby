059f3bf4d904baf8cc82a3070314dc963b7d1b31
CRClothier <c.r.clothier@gmail.com>
2022-05-31 11:51:51 -0500

    Fix linters

--- models/author.rb
+++ models/author.rb
@@ -2,7 +2,7 @@ class Author
   attr_accessor :first_name, :last_name
 
   def initialize(first_name, last_name, id: nil)
-    @id id.nil? ? Time.now.to_i : id
+    @id = id.nil? ? Time.now.to_i : id
     @first_name = first_name
     @last_name = last_name
     @items = []
