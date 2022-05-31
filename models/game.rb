7ceb2f924ddf7507f233bc03d951a87fd5828901
CRClothier <c.r.clothier@gmail.com>
2022-05-31 11:16:03 -0500

    Implement Game class properties in constructor

--- models/game.rb
+++ models/game.rb
@@ -1,4 +1,9 @@
 require ',/item'
 
 class Game < item
+  attr_accessor :multiplayer :last_played_at
+  def initialize(date, archived, multiplayer, last_played_at, id: nil)
+    super(date, archived, id: id)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
 end
