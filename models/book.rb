e2b287851556092557f64c1ff6499497741032be
JuanLP <juan.luis.palacios.p@gmail.com>
2022-06-01 17:22:29 -0500

    Add label json serialization

--- models/book.rb
+++ models/book.rb
@@ -22,6 +22,7 @@ class Book < Item
     new_instance = new(publish_date, archived, publisher, cover_state, id: id)
     found_label = labels.find { |label| label.id == id }
     found_label&.add_item(label)
+    
     new_instance
   end
 end
