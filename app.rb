03cbc814adee44207328869ca1284073da53c043
JuanLP <juan.luis.palacios.p@gmail.com>
2022-06-01 12:11:12 -0500

    Add UI method add_a_book

--- app.rb
+++ app.rb
@@ -2,12 +2,28 @@
 require_relative './movie'
 require 'date'
 require_relative './models/music_album'
+require './models/book'
+require './models/label'
+
+COLOR_CODES = {
+  'black' => 30,
+  'red' => 31,
+  'green' => 32,
+  'yellow' => 33,
+  'blue' => 34,
+  'pink' => 35,
+  'cyan' => 36,
+  'white' => 37,
+  'default' => 39
+}.freeze
 
 class App
   def initialize
     @movies = []
     @sources = []
     @music_albums = []
+    @books = []
+    @labels = [Label.new('Broken', 'red')]
   end
 
   def list_all_books
@@ -51,8 +67,38 @@ class App
     end
   end
 
+  def select_label
+    puts "  \t|id\t\t|title\t\t|color\n#{['-'] * 50 * ''}"
+    @labels.each_with_index do |label, i|
+      puts "#{i})\t#{label.id}\t#{label.title}\t\t\033[#{COLOR_CODES[label.color]}m#{label.color}\033[0m"
+    end
+    puts "#{@labels.length})\t+ Add label"
+    puts 'Select a label:'
+    label = @labels[gets.to_i]
+    if label.nil?
+      puts 'Tile:'
+      title = gets.chomp
+      puts 'Color(black/red/green/yellow/blue/pink/cyan/white/default):'
+      color = gets.chomp
+      color = 'default' if COLOR_CODES[color].nil?
+      label = Label.new(title, color)
+    end
+    @labels << label
+    label
+  end
+
   def add_a_book
-    raise StandardError, 'not implemented'
+    puts 'Publish date (YYYY-MM-DD):'
+    date = Date.parse(gets.chomp)
+    puts 'Is it archived(y/n)?:'
+    archived = gets.chomp == 'y'
+    puts 'Publisher:'
+    publisher = gets.chomp
+    puts 'Cover state:'
+    cover_state = gets.chomp
+    book = Book.new(date, archived, publisher, cover_state)
+    label = select_label
+    label.add_item(book)
   end
 
   def add_a_music_album
