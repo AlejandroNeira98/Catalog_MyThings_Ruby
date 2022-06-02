d9851dce19a31c16dc74d490eb6c189c7f4b4b80
CRClothier <c.r.clothier@gmail.com>
2022-06-01 23:18:00 -0500

    Fix linters

--- app.rb
+++ app.rb
@@ -6,7 +6,6 @@ require './models/game'
 require './models/author'
 require './models/book'
 require './models/label'
-require './models/game'
 
 COLOR_CODES = {
   'black' => 30,
@@ -152,18 +151,18 @@ class App
   def add_a_game
     puts 'Insert publish date (in the format of YYYY/MM/DD)'
     published = gets.chomp
-    published = Date.parse(date)
+    published = Date.parse(published)
     puts 'Is the game archived? (y/n)'
     archived = gets.chomp
     archived = %w[Y y].include?(archived)
     puts 'Is the game multiplayer? (y/n)'
     multiplayer = gets.chomp
-    multiplayer = %w[Y y].include?(silent)
+    multiplayer = %w[Y y].include?(multiplayer)
     puts 'Insert date you last played (in the format of YYYY/MM/DD)'
     last_played = gets.chomp
-    last_played = Date.parse(date)
+    last_played = Date.parse(last_played)
     game = Game.new(date, archived, multiplayer, last_played)
-    @movies << movie
+    @game << game
   end
 
   def save
